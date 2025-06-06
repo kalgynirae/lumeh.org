abd2105b5f2234cfa27f79693b371fefaca03534
Colin Chan <colin+git@lumeh.org>
2025-06-06 12:46:50 -0700

    Redesign anchor links

--- websleydale/__init__.py
+++ websleydale/__init__.py
@@ -541,7 +541,7 @@ class WebsleydaleHTMLRenderer(HTMLRenderer):
         inner = self.render_inner(token)
         if level > 1:
             identifier = self.ids.get_id(self.render_to_plain(token))
-            return f'<a class=anchor href="#{identifier}"><h{level} id="{identifier}">{inner}</h{level}></a>'
+            return f'<div id="{identifier}" class=anchor><a class=anchor-button href="#{identifier}"><l-icon name=anchor right></l-icon></a><h{level}>{inner}</h{level}></div>'
         else:
             return f"<h{level}>{inner}</h{level}>"
 
